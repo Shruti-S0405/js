791396547dc51b893d1de884f612bc90efc3aa9f
Shruti <shruti.sundar0405@gmail.com>
2024-01-06 09:47:57 +0530

    initial commit

---
+++ home.js
@@ -0,0 +1,17 @@
+// console.log("Hello");
+// //alert("Hello this is shruti");
+// var b="smoothie";
+// console.log(b);
+// var num=45;
+// console.log(num);
+
+// var age= prompt("What is your age?");
+// document.getElementById('someText').innerHTML=age;
+//function
+//1.create a function, 2.Call the function
+function name(){
+    var name=prompt("What is your name?");
+    var result="Hello"+' '+name;
+    console.log(result);
+}
+name();
